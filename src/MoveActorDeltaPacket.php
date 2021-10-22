pmmp/BedrockProtocol

commit fbd701bae86eded8a9953118c425e1777220a53b

Merge branch 'stable'

src/MoveActorDeltaPacket.php
@@ -45,12 +45,12 @@ class MoveActorDeltaPacket extends DataPacket implements ClientboundPacket{
 	public $entityRuntimeId;
 	/** @var int */
 	public $flags;
-	/** @var int */
-	public $xDiff = 0;
-	/** @var int */
-	public $yDiff = 0;
-	/** @var int */
-	public $zDiff = 0;
+	/** @var float */
+	public $xPos = 0;
+	/** @var float */
+	public $yPos = 0;
+	/** @var float */
+	public $zPos = 0;
 	/** @var float */
 	public $xRot = 0.0;
 	/** @var float */
@@ -61,9 +61,9 @@ class MoveActorDeltaPacket extends DataPacket implements ClientboundPacket{
 	/**
 	 * @throws BinaryDataException
 	 */
-	private function maybeReadCoord(int $flag, PacketSerializer $in) : int{
+	private function maybeReadCoord(int $flag, PacketSerializer $in) : float{
 		if(($this->flags & $flag) !== 0){
-			return $in->getVarInt();
+			return $in->getLFloat();
 		}
 		return 0;
 	}
@@ -81,17 +81,17 @@ class MoveActorDeltaPacket extends DataPacket implements ClientboundPacket{
 	protected function decodePayload(PacketSerializer $in) : void{
 		$this->entityRuntimeId = $in->getEntityRuntimeId();
 		$this->flags = $in->getLShort();
-		$this->xDiff = $this->maybeReadCoord(self::FLAG_HAS_X, $in);
-		$this->yDiff = $this->maybeReadCoord(self::FLAG_HAS_Y, $in);
-		$this->zDiff = $this->maybeReadCoord(self::FLAG_HAS_Z, $in);
+		$this->xPos = $this->maybeReadCoord(self::FLAG_HAS_X, $in);
+		$this->yPos = $this->maybeReadCoord(self::FLAG_HAS_Y, $in);
+		$this->zPos = $this->maybeReadCoord(self::FLAG_HAS_Z, $in);
 		$this->xRot = $this->maybeReadRotation(self::FLAG_HAS_ROT_X, $in);
 		$this->yRot = $this->maybeReadRotation(self::FLAG_HAS_ROT_Y, $in);
 		$this->zRot = $this->maybeReadRotation(self::FLAG_HAS_ROT_Z, $in);
 	}
 
-	private function maybeWriteCoord(int $flag, int $val, PacketSerializer $out) : void{
+	private function maybeWriteCoord(int $flag, float $val, PacketSerializer $out) : void{
 		if(($this->flags & $flag) !== 0){
-			$out->putVarInt($val);
+			$out->putLFloat($val);
 		}
 	}
 
@@ -104,15 +104,15 @@ class MoveActorDeltaPacket extends DataPacket implements ClientboundPacket{
 	protected function encodePayload(PacketSerializer $out) : void{
 		$out->putEntityRuntimeId($this->entityRuntimeId);
 		$out->putLShort($this->flags);
-		$this->maybeWriteCoord(self::FLAG_HAS_X, $this->xDiff, $out);
-		$this->maybeWriteCoord(self::FLAG_HAS_Y, $this->yDiff, $out);
-		$this->maybeWriteCoord(self::FLAG_HAS_Z, $this->zDiff, $out);
+		$this->maybeWriteCoord(self::FLAG_HAS_X, $this->xPos, $out);
+		$this->maybeWriteCoord(self::FLAG_HAS_Y, $this->yPos, $out);
+		$this->maybeWriteCoord(self::FLAG_HAS_Z, $this->zPos, $out);
 		$this->maybeWriteRotation(self::FLAG_HAS_ROT_X, $this->xRot, $out);
 		$this->maybeWriteRotation(self::FLAG_HAS_ROT_Y, $this->yRot, $out);
 		$this->maybeWriteRotation(self::FLAG_HAS_ROT_Z, $this->zRot, $out);
 	}
 
-	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handleMoveActorDelta($this);
+	public function handle(PacketHandlerInterface $session) : bool{
+		return $session->handleMoveActorDelta($this);
 	}
 }

src/AnimateEntityPacket.php
@@ -1,0 +1,108 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol;
+
+#include <rules/DataPacket.h>
+
+use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
+use function count;
+
+class AnimateEntityPacket extends DataPacket implements ClientboundPacket{
+	public const NETWORK_ID = ProtocolInfo::ANIMATE_ENTITY_PACKET;
+
+	/** @var string */
+	private $animation;
+	/** @var string */
+	private $nextState;
+	/** @var string */
+	private $stopExpression;
+	/** @var string */
+	private $controller;
+	/** @var float */
+	private $blendOutTime;
+	/**
+	 * @var int[]
+	 * @phpstan-var list<int>
+	 */
+	private $actorRuntimeIds;
+
+	/**
+	 * @param int[] $actorRuntimeIds
+	 * @phpstan-param list<int> $actorRuntimeIds
+	 */
+	public static function create(string $animation, string $nextState, string $stopExpression, string $controller, float $blendOutTime, array $actorRuntimeIds) : self{
+		$result = new self;
+		$result->animation = $animation;
+		$result->nextState = $nextState;
+		$result->stopExpression = $stopExpression;
+		$result->controller = $controller;
+		$result->blendOutTime = $blendOutTime;
+		$result->actorRuntimeIds = $actorRuntimeIds;
+		return $result;
+	}
+
+	public function getAnimation() : string{ return $this->animation; }
+
+	public function getNextState() : string{ return $this->nextState; }
+
+	public function getStopExpression() : string{ return $this->stopExpression; }
+
+	public function getController() : string{ return $this->controller; }
+
+	public function getBlendOutTime() : float{ return $this->blendOutTime; }
+
+	/**
+	 * @return int[]
+	 * @phpstan-return list<int>
+	 */
+	public function getActorRuntimeIds() : array{ return $this->actorRuntimeIds; }
+
+	protected function decodePayload(PacketSerializer $in) : void{
+		$this->animation = $in->getString();
+		$this->nextState = $in->getString();
+		$this->stopExpression = $in->getString();
+		$this->controller = $in->getString();
+		$this->blendOutTime = $in->getLFloat();
+		$this->actorRuntimeIds = [];
+		for($i = 0, $len = $in->getUnsignedVarInt(); $i < $len; ++$i){
+			$this->actorRuntimeIds[] = $in->getEntityRuntimeId();
+		}
+	}
+
+	protected function encodePayload(PacketSerializer $out) : void{
+		$out->putString($this->animation);
+		$out->putString($this->nextState);
+		$out->putString($this->stopExpression);
+		$out->putString($this->controller);
+		$out->putLFloat($this->blendOutTime);
+		$out->putUnsignedVarInt(count($this->actorRuntimeIds));
+		foreach($this->actorRuntimeIds as $id){
+			$out->putEntityRuntimeId($id);
+		}
+	}
+
+	public function handle(PacketHandlerInterface $handler) : bool{
+		return $handler->handleAnimateEntity($this);
+	}
+}

src/CameraShakePacket.php
@@ -25,41 +25,48 @@ namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
-use pocketmine\network\mcpe\protocol\types\CacheableNbt;
 
-class UpdateBlockPropertiesPacket extends DataPacket implements ClientboundPacket{
-	public const NETWORK_ID = ProtocolInfo::UPDATE_BLOCK_PROPERTIES_PACKET;
+class CameraShakePacket extends DataPacket implements ClientboundPacket{
+	public const NETWORK_ID = ProtocolInfo::CAMERA_SHAKE_PACKET;
 
-	/**
-	 * @var CacheableNbt
-	 * @phpstan-var CacheableNbt<\pocketmine\nbt\tag\CompoundTag>
-	 */
-	private $blockProperties;
+	public const TYPE_POSITIONAL = 0;
+	public const TYPE_ROTATIONAL = 1;
 
-	public static function create(CompoundTag $data) : self{
+	/** @var float */
+	private $intensity;
+	/** @var float */
+	private $duration;
+	/** @var int */
+	private $shakeType;
+
+	public static function create(float $intensity, float $duration, int $shakeType) : self{
 		$result = new self;
-		$result->blockProperties = new CacheableNbt($data);
+		$result->intensity = $intensity;
+		$result->duration = $duration;
+		$result->shakeType = $shakeType;
 		return $result;
 	}
 
-	/**
-	 * @phpstan-return CacheableNbt<\pocketmine\nbt\tag\CompoundTag>
-	 */
-	public function getBlockProperties() : CacheableNbt{
-		return $this->blockProperties;
-	}
+	public function getIntensity() : float{ return $this->intensity; }
+
+	public function getDuration() : float{ return $this->duration; }
+
+	public function getShakeType() : int{ return $this->shakeType; }
 
 	protected function decodePayload(PacketSerializer $in) : void{
-		$this->blockProperties = new CacheableNbt($in->getNbtCompoundRoot());
+		$this->intensity = $in->getLFloat();
+		$this->duration = $in->getLFloat();
+		$this->shakeType = $in->getByte();
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
-		$out->put($this->blockProperties->getEncodedNbt());
+		$out->putLFloat($this->intensity);
+		$out->putLFloat($this->duration);
+		$out->putByte($this->shakeType);
 	}
 
 	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handleUpdateBlockProperties($this);
+		return $handler->handleCameraShake($this);
 	}
 }

src/ContainerClosePacket.php
@@ -32,19 +32,24 @@ class ContainerClosePacket extends DataPacket implements ClientboundPacket, Serv
 
 	/** @var int */
 	public $windowId;
+	/** @var bool */
+	public $server = false;
 
-	public static function create(int $windowId) : self{
+	public static function create(int $windowId, bool $server) : self{
 		$result = new self;
 		$result->windowId = $windowId;
+		$result->server = $server;
 		return $result;
 	}
 
 	protected function decodePayload(PacketSerializer $in) : void{
 		$this->windowId = $in->getByte();
+		$this->server = $in->getBool();
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
 		$out->putByte($this->windowId);
+		$out->putBool($this->server);
 	}
 
 	public function handle(PacketHandlerInterface $handler) : bool{

src/CorrectPlayerMovePredictionPacket.php
@@ -25,46 +25,53 @@ namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
+use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
-use pocketmine\network\mcpe\protocol\types\entity\Attribute;
-use function array_values;
 
-class UpdateAttributesPacket extends DataPacket implements ClientboundPacket{
-	public const NETWORK_ID = ProtocolInfo::UPDATE_ATTRIBUTES_PACKET;
+class CorrectPlayerMovePredictionPacket extends DataPacket implements ClientboundPacket{
+	public const NETWORK_ID = ProtocolInfo::CORRECT_PLAYER_MOVE_PREDICTION_PACKET;
 
+	/** @var Vector3 */
+	private $position;
+	/** @var Vector3 */
+	private $delta;
+	/** @var bool */
+	private $onGround;
 	/** @var int */
-	public $entityRuntimeId;
-	/** @var Attribute[] */
-	public $entries = [];
-	/** @var int */
-	public $tick = 0;
+	private $tick;
 
-	/**
-	 * @param Attribute[] $attributes
-	 *
-	 * @return UpdateAttributesPacket
-	 */
-	public static function create(int $entityRuntimeId, array $attributes, int $tick) : self{
+	public static function create(Vector3 $position, Vector3 $delta, bool $onGround, int $tick) : self{
 		$result = new self;
-		$result->entityRuntimeId = $entityRuntimeId;
-		$result->entries = $attributes;
+		$result->position = $position;
+		$result->delta = $delta;
+		$result->onGround = $onGround;
 		$result->tick = $tick;
 		return $result;
 	}
 
+	public function getPosition() : Vector3{ return $this->position; }
+
+	public function getDelta() : Vector3{ return $this->delta; }
+
+	public function isOnGround() : bool{ return $this->onGround; }
+
+	public function getTick() : int{ return $this->tick; }
+
 	protected function decodePayload(PacketSerializer $in) : void{
-		$this->entityRuntimeId = $in->getEntityRuntimeId();
-		$this->entries = $in->getAttributeList();
+		$this->position = $in->getVector3();
+		$this->delta = $in->getVector3();
+		$this->onGround = $in->getBool();
 		$this->tick = $in->getUnsignedVarLong();
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
-		$out->putEntityRuntimeId($this->entityRuntimeId);
-		$out->putAttributeList(...array_values($this->entries));
+		$out->putVector3($this->position);
+		$out->putVector3($this->delta);
+		$out->putBool($this->onGround);
 		$out->putUnsignedVarLong($this->tick);
 	}
 
 	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handleUpdateAttributes($this);
+		return $handler->handleCorrectPlayerMovePrediction($this);
 	}
 }

src/ItemComponentPacket.php
@@ -1,0 +1,79 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol;
+
+#include <rules/DataPacket.h>
+
+use pocketmine\nbt\TreeRoot;
+use pocketmine\network\mcpe\protocol\serializer\NetworkNbtSerializer;
+use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
+use pocketmine\network\mcpe\protocol\types\ItemComponentPacketEntry;
+use function count;
+
+class ItemComponentPacket extends DataPacket implements ClientboundPacket{
+	public const NETWORK_ID = ProtocolInfo::ITEM_COMPONENT_PACKET;
+
+	/**
+	 * @var ItemComponentPacketEntry[]
+	 * @phpstan-var list<ItemComponentPacketEntry>
+	 */
+	private $entries;
+
+	/**
+	 * @param ItemComponentPacketEntry[] $entries
+	 * @phpstan-param list<ItemComponentPacketEntry> $entries
+	 */
+	public static function create(array $entries) : self{
+		$result = new self;
+		$result->entries = $entries;
+		return $result;
+	}
+
+	/**
+	 * @return ItemComponentPacketEntry[]
+	 * @phpstan-return list<ItemComponentPacketEntry>
+	 */
+	public function getEntries() : array{ return $this->entries; }
+
+	protected function decodePayload(PacketSerializer $in) : void{
+		$this->entries = [];
+		for($i = 0, $len = $in->getUnsignedVarInt(); $i < $len; ++$i){
+			$name = $in->getString();
+			$nbt = $in->getNbtCompoundRoot();
+			$this->entries[] = new ItemComponentPacketEntry($name, $nbt);
+		}
+	}
+
+	protected function encodePayload(PacketSerializer $out) : void{
+		$out->putUnsignedVarInt(count($this->entries));
+		foreach($this->entries as $entry){
+			$out->putString($entry->getName());
+			$out->put((new NetworkNbtSerializer())->write(new TreeRoot($entry->getComponentNbt())));
+		}
+	}
+
+	public function handle(PacketHandlerInterface $handler) : bool{
+		return $handler->handleItemComponent($this);
+	}
+}

src/MotionPredictionHintsPacket.php
@@ -25,31 +25,46 @@ namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
+use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 
-class ActorFallPacket extends DataPacket implements ServerboundPacket{
-	public const NETWORK_ID = ProtocolInfo::ACTOR_FALL_PACKET;
+class MotionPredictionHintsPacket extends DataPacket implements ClientboundPacket{
+	public const NETWORK_ID = ProtocolInfo::MOTION_PREDICTION_HINTS_PACKET;
 
 	/** @var int */
-	public $entityRuntimeId;
-	/** @var float */
-	public $fallDistance;
+	private $entityRuntimeId;
+	/** @var Vector3 */
+	private $motion;
 	/** @var bool */
-	public $isInVoid;
+	private $onGround;
+
+	public static function create(int $entityRuntimeId, Vector3 $motion, bool $onGround) : self{
+		$result = new self;
+		$result->entityRuntimeId = $entityRuntimeId;
+		$result->motion = $motion;
+		$result->onGround = $onGround;
+		return $result;
+	}
+
+	public function getEntityRuntimeId() : int{ return $this->entityRuntimeId; }
+
+	public function getMotion() : Vector3{ return $this->motion; }
+
+	public function isOnGround() : bool{ return $this->onGround; }
 
 	protected function decodePayload(PacketSerializer $in) : void{
 		$this->entityRuntimeId = $in->getEntityRuntimeId();
-		$this->fallDistance = $in->getLFloat();
-		$this->isInVoid = $in->getBool();
+		$this->motion = $in->getVector3();
+		$this->onGround = $in->getBool();
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
 		$out->putEntityRuntimeId($this->entityRuntimeId);
-		$out->putLFloat($this->fallDistance);
-		$out->putBool($this->isInVoid);
+		$out->putVector3($this->motion);
+		$out->putBool($this->onGround);
 	}
 
 	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handleActorFall($this);
+		return $handler->handleMotionPredictionHints($this);
 	}
 }

src/MovePlayerPacket.php
@@ -56,6 +56,8 @@ class MovePlayerPacket extends DataPacket implements ClientboundPacket, Serverbo
 	public $teleportCause = 0;
 	/** @var int */
 	public $teleportItem = 0;
+	/** @var int */
+	public $tick = 0;
 
 	protected function decodePayload(PacketSerializer $in) : void{
 		$this->entityRuntimeId = $in->getEntityRuntimeId();
@@ -70,6 +72,7 @@ class MovePlayerPacket extends DataPacket implements ClientboundPacket, Serverbo
 			$this->teleportCause = $in->getLInt();
 			$this->teleportItem = $in->getLInt();
 		}
+		$this->tick = $in->getUnsignedVarLong();
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
@@ -85,6 +88,7 @@ class MovePlayerPacket extends DataPacket implements ClientboundPacket, Serverbo
 			$out->putLInt($this->teleportCause);
 			$out->putLInt($this->teleportItem);
 		}
+		$out->putUnsignedVarLong($this->tick);
 	}
 
 	public function handle(PacketHandlerInterface $handler) : bool{

commit 0a248d4b0f42daa7bae77a77469dbc9d5360cb39

Fixed merge error introduced by fbd701bae86eded8a9953118c425e1777220a53b

src/MoveActorDeltaPacket.php
@@ -104,7 +104,7 @@ class MoveActorDeltaPacket extends DataPacket implements ClientboundPacket{
 		$this->maybeWriteRotation(self::FLAG_HAS_HEAD_YAW, $this->headYaw, $out);
 	}
 
-	public function handle(PacketHandlerInterface $session) : bool{
-		return $session->handleMoveActorDelta($this);
+	public function handle(PacketHandlerInterface $handler) : bool{
+		return $handler->handleMoveActorDelta($this);
 	}
 }
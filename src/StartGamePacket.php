68ffdec5144d70abdd96389557d993d97df80855
Dylan K. Taylor <dktapps@pmmp.io>
2021-10-22 21:40:53 +0100

    Extract LevelSettings from StartGamePacket this makes StartGame much less of a monstrosity - now LevelSettings is the monstrosity instead :D

--- src/StartGamePacket.php
+++ src/StartGamePacket.php
@@ -30,17 +30,9 @@ use pocketmine\nbt\TreeRoot;
 use pocketmine\network\mcpe\protocol\serializer\NetworkNbtSerializer;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 use pocketmine\network\mcpe\protocol\types\BlockPaletteEntry;
-use pocketmine\network\mcpe\protocol\types\BlockPosition;
-use pocketmine\network\mcpe\protocol\types\EducationEditionOffer;
-use pocketmine\network\mcpe\protocol\types\EducationUriResource;
-use pocketmine\network\mcpe\protocol\types\Experiments;
-use pocketmine\network\mcpe\protocol\types\GameRule;
-use pocketmine\network\mcpe\protocol\types\GeneratorType;
 use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
-use pocketmine\network\mcpe\protocol\types\MultiplayerGameVisibility;
+use pocketmine\network\mcpe\protocol\types\LevelSettings;
 use pocketmine\network\mcpe\protocol\types\PlayerMovementSettings;
-use pocketmine\network\mcpe\protocol\types\PlayerPermissions;
-use pocketmine\network\mcpe\protocol\types\SpawnSettings;
 use function count;
 
 class StartGamePacket extends DataPacket implements ClientboundPacket{
@@ -55,51 +47,7 @@ class StartGamePacket extends DataPacket implements ClientboundPacket{
 	public float $pitch;
 	public float $yaw;
 
-	public int $seed;
-	public SpawnSettings $spawnSettings;
-	public int $generator = GeneratorType::OVERWORLD;
-	public int $worldGamemode;
-	public int $difficulty;
-	public BlockPosition $spawnPosition;
-	public bool $hasAchievementsDisabled = true;
-	public int $time = -1;
-	public int $eduEditionOffer = EducationEditionOffer::NONE;
-	public bool $hasEduFeaturesEnabled = false;
-	public string $eduProductUUID = "";
-	public float $rainLevel;
-	public float $lightningLevel;
-	public bool $hasConfirmedPlatformLockedContent = false;
-	public bool $isMultiplayerGame = true;
-	public bool $hasLANBroadcast = true;
-	public int $xboxLiveBroadcastMode = MultiplayerGameVisibility::PUBLIC;
-	public int $platformBroadcastMode = MultiplayerGameVisibility::PUBLIC;
-	public bool $commandsEnabled;
-	public bool $isTexturePacksRequired = true;
-	/**
-	 * @var GameRule[]
-	 * @phpstan-var array<string, GameRule>
-	 */
-	public array $gameRules = [];
-	public Experiments $experiments;
-	public bool $hasBonusChestEnabled = false;
-	public bool $hasStartWithMapEnabled = false;
-	public int $defaultPlayerPermission = PlayerPermissions::MEMBER; //TODO
-
-	public int $serverChunkTickRadius = 4; //TODO (leave as default for now)
-
-	public bool $hasLockedBehaviorPack = false;
-	public bool $hasLockedResourcePack = false;
-	public bool $isFromLockedWorldTemplate = false;
-	public bool $useMsaGamertagsOnly = false;
-	public bool $isFromWorldTemplate = false;
-	public bool $isWorldTemplateOptionLocked = false;
-	public bool $onlySpawnV1Villagers = false;
-	public string $vanillaVersion = ProtocolInfo::MINECRAFT_VERSION_NETWORK;
-	public int $limitedWorldWidth = 0;
-	public int $limitedWorldLength = 0;
-	public bool $isNewNether = true;
-	public ?EducationUriResource $eduSharedUriResource = null;
-	public ?bool $experimentalGameplayOverride = null;
+	public LevelSettings $levelSettings;
 
 	public string $levelId = ""; //base64 string, usually the same as world folder name in vanilla
 	public string $worldName;
@@ -133,50 +81,7 @@ class StartGamePacket extends DataPacket implements ClientboundPacket{
 		$this->pitch = $in->getLFloat();
 		$this->yaw = $in->getLFloat();
 
-		//Level settings
-		$this->seed = $in->getVarInt();
-		$this->spawnSettings = SpawnSettings::read($in);
-		$this->generator = $in->getVarInt();
-		$this->worldGamemode = $in->getVarInt();
-		$this->difficulty = $in->getVarInt();
-		$this->spawnPosition = $in->getBlockPosition();
-		$this->hasAchievementsDisabled = $in->getBool();
-		$this->time = $in->getVarInt();
-		$this->eduEditionOffer = $in->getVarInt();
-		$this->hasEduFeaturesEnabled = $in->getBool();
-		$this->eduProductUUID = $in->getString();
-		$this->rainLevel = $in->getLFloat();
-		$this->lightningLevel = $in->getLFloat();
-		$this->hasConfirmedPlatformLockedContent = $in->getBool();
-		$this->isMultiplayerGame = $in->getBool();
-		$this->hasLANBroadcast = $in->getBool();
-		$this->xboxLiveBroadcastMode = $in->getVarInt();
-		$this->platformBroadcastMode = $in->getVarInt();
-		$this->commandsEnabled = $in->getBool();
-		$this->isTexturePacksRequired = $in->getBool();
-		$this->gameRules = $in->getGameRules();
-		$this->experiments = Experiments::read($in);
-		$this->hasBonusChestEnabled = $in->getBool();
-		$this->hasStartWithMapEnabled = $in->getBool();
-		$this->defaultPlayerPermission = $in->getVarInt();
-		$this->serverChunkTickRadius = $in->getLInt();
-		$this->hasLockedBehaviorPack = $in->getBool();
-		$this->hasLockedResourcePack = $in->getBool();
-		$this->isFromLockedWorldTemplate = $in->getBool();
-		$this->useMsaGamertagsOnly = $in->getBool();
-		$this->isFromWorldTemplate = $in->getBool();
-		$this->isWorldTemplateOptionLocked = $in->getBool();
-		$this->onlySpawnV1Villagers = $in->getBool();
-		$this->vanillaVersion = $in->getString();
-		$this->limitedWorldWidth = $in->getLInt();
-		$this->limitedWorldLength = $in->getLInt();
-		$this->isNewNether = $in->getBool();
-		$this->eduSharedUriResource = EducationUriResource::read($in);
-		if($in->getBool()){
-			$this->experimentalGameplayOverride = $in->getBool();
-		}else{
-			$this->experimentalGameplayOverride = null;
-		}
+		$this->levelSettings = LevelSettings::read($in);
 
 		$this->levelId = $in->getString();
 		$this->worldName = $in->getString();
@@ -218,49 +123,7 @@ class StartGamePacket extends DataPacket implements ClientboundPacket{
 		$out->putLFloat($this->pitch);
 		$out->putLFloat($this->yaw);
 
-		//Level settings
-		$out->putVarInt($this->seed);
-		$this->spawnSettings->write($out);
-		$out->putVarInt($this->generator);
-		$out->putVarInt($this->worldGamemode);
-		$out->putVarInt($this->difficulty);
-		$out->putBlockPosition($this->spawnPosition);
-		$out->putBool($this->hasAchievementsDisabled);
-		$out->putVarInt($this->time);
-		$out->putVarInt($this->eduEditionOffer);
-		$out->putBool($this->hasEduFeaturesEnabled);
-		$out->putString($this->eduProductUUID);
-		$out->putLFloat($this->rainLevel);
-		$out->putLFloat($this->lightningLevel);
-		$out->putBool($this->hasConfirmedPlatformLockedContent);
-		$out->putBool($this->isMultiplayerGame);
-		$out->putBool($this->hasLANBroadcast);
-		$out->putVarInt($this->xboxLiveBroadcastMode);
-		$out->putVarInt($this->platformBroadcastMode);
-		$out->putBool($this->commandsEnabled);
-		$out->putBool($this->isTexturePacksRequired);
-		$out->putGameRules($this->gameRules);
-		$this->experiments->write($out);
-		$out->putBool($this->hasBonusChestEnabled);
-		$out->putBool($this->hasStartWithMapEnabled);
-		$out->putVarInt($this->defaultPlayerPermission);
-		$out->putLInt($this->serverChunkTickRadius);
-		$out->putBool($this->hasLockedBehaviorPack);
-		$out->putBool($this->hasLockedResourcePack);
-		$out->putBool($this->isFromLockedWorldTemplate);
-		$out->putBool($this->useMsaGamertagsOnly);
-		$out->putBool($this->isFromWorldTemplate);
-		$out->putBool($this->isWorldTemplateOptionLocked);
-		$out->putBool($this->onlySpawnV1Villagers);
-		$out->putString($this->vanillaVersion);
-		$out->putLInt($this->limitedWorldWidth);
-		$out->putLInt($this->limitedWorldLength);
-		$out->putBool($this->isNewNether);
-		($this->eduSharedUriResource ?? new EducationUriResource("", ""))->write($out);
-		$out->putBool($this->experimentalGameplayOverride !== null);
-		if($this->experimentalGameplayOverride !== null){
-			$out->putBool($this->experimentalGameplayOverride);
-		}
+		$this->levelSettings->write($out);
 
 		$out->putString($this->levelId);
 		$out->putString($this->worldName);
---
+++ src/types/LevelSettings.php
@@ -0,0 +1,185 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol\types;
+
+use pocketmine\network\mcpe\protocol\PacketDecodeException;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
+use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
+use pocketmine\utils\BinaryDataException;
+
+final class LevelSettings{
+
+	public int $seed;
+	public SpawnSettings $spawnSettings;
+	public int $generator = GeneratorType::OVERWORLD;
+	public int $worldGamemode;
+	public int $difficulty;
+	public BlockPosition $spawnPosition;
+	public bool $hasAchievementsDisabled = true;
+	public int $time = -1;
+	public int $eduEditionOffer = EducationEditionOffer::NONE;
+	public bool $hasEduFeaturesEnabled = false;
+	public string $eduProductUUID = "";
+	public float $rainLevel;
+	public float $lightningLevel;
+	public bool $hasConfirmedPlatformLockedContent = false;
+	public bool $isMultiplayerGame = true;
+	public bool $hasLANBroadcast = true;
+	public int $xboxLiveBroadcastMode = MultiplayerGameVisibility::PUBLIC;
+	public int $platformBroadcastMode = MultiplayerGameVisibility::PUBLIC;
+	public bool $commandsEnabled;
+	public bool $isTexturePacksRequired = true;
+	/**
+	 * @var GameRule[]
+	 * @phpstan-var array<string, GameRule>
+	 */
+	public array $gameRules = [];
+	public Experiments $experiments;
+	public bool $hasBonusChestEnabled = false;
+	public bool $hasStartWithMapEnabled = false;
+	public int $defaultPlayerPermission = PlayerPermissions::MEMBER; //TODO
+
+	public int $serverChunkTickRadius = 4; //TODO (leave as default for now)
+
+	public bool $hasLockedBehaviorPack = false;
+	public bool $hasLockedResourcePack = false;
+	public bool $isFromLockedWorldTemplate = false;
+	public bool $useMsaGamertagsOnly = false;
+	public bool $isFromWorldTemplate = false;
+	public bool $isWorldTemplateOptionLocked = false;
+	public bool $onlySpawnV1Villagers = false;
+	public string $vanillaVersion = ProtocolInfo::MINECRAFT_VERSION_NETWORK;
+	public int $limitedWorldWidth = 0;
+	public int $limitedWorldLength = 0;
+	public bool $isNewNether = true;
+	public ?EducationUriResource $eduSharedUriResource = null;
+	public ?bool $experimentalGameplayOverride = null;
+
+	/**
+	 * @throws BinaryDataException
+	 * @throws PacketDecodeException
+	 */
+	public static function read(PacketSerializer $in) : self{
+		//TODO: in the future we'll use promoted properties + named arguments for decoding, but for now we stick with
+		//this shitty way to limit BC breaks (needs more R&D)
+		$result = new self;
+		$result->internalRead($in);
+		return $result;
+	}
+
+	/**
+	 * @throws BinaryDataException
+	 * @throws PacketDecodeException
+	 */
+	private function internalRead(PacketSerializer $in) : void{
+		$this->seed = $in->getVarInt();
+		$this->spawnSettings = SpawnSettings::read($in);
+		$this->generator = $in->getVarInt();
+		$this->worldGamemode = $in->getVarInt();
+		$this->difficulty = $in->getVarInt();
+		$this->spawnPosition = $in->getBlockPosition();
+		$this->hasAchievementsDisabled = $in->getBool();
+		$this->time = $in->getVarInt();
+		$this->eduEditionOffer = $in->getVarInt();
+		$this->hasEduFeaturesEnabled = $in->getBool();
+		$this->eduProductUUID = $in->getString();
+		$this->rainLevel = $in->getLFloat();
+		$this->lightningLevel = $in->getLFloat();
+		$this->hasConfirmedPlatformLockedContent = $in->getBool();
+		$this->isMultiplayerGame = $in->getBool();
+		$this->hasLANBroadcast = $in->getBool();
+		$this->xboxLiveBroadcastMode = $in->getVarInt();
+		$this->platformBroadcastMode = $in->getVarInt();
+		$this->commandsEnabled = $in->getBool();
+		$this->isTexturePacksRequired = $in->getBool();
+		$this->gameRules = $in->getGameRules();
+		$this->experiments = Experiments::read($in);
+		$this->hasBonusChestEnabled = $in->getBool();
+		$this->hasStartWithMapEnabled = $in->getBool();
+		$this->defaultPlayerPermission = $in->getVarInt();
+		$this->serverChunkTickRadius = $in->getLInt();
+		$this->hasLockedBehaviorPack = $in->getBool();
+		$this->hasLockedResourcePack = $in->getBool();
+		$this->isFromLockedWorldTemplate = $in->getBool();
+		$this->useMsaGamertagsOnly = $in->getBool();
+		$this->isFromWorldTemplate = $in->getBool();
+		$this->isWorldTemplateOptionLocked = $in->getBool();
+		$this->onlySpawnV1Villagers = $in->getBool();
+		$this->vanillaVersion = $in->getString();
+		$this->limitedWorldWidth = $in->getLInt();
+		$this->limitedWorldLength = $in->getLInt();
+		$this->isNewNether = $in->getBool();
+		$this->eduSharedUriResource = EducationUriResource::read($in);
+		if($in->getBool()){
+			$this->experimentalGameplayOverride = $in->getBool();
+		}else{
+			$this->experimentalGameplayOverride = null;
+		}
+	}
+
+	public function write(PacketSerializer $out) : void{
+		$out->putVarInt($this->seed);
+		$this->spawnSettings->write($out);
+		$out->putVarInt($this->generator);
+		$out->putVarInt($this->worldGamemode);
+		$out->putVarInt($this->difficulty);
+		$out->putBlockPosition($this->spawnPosition);
+		$out->putBool($this->hasAchievementsDisabled);
+		$out->putVarInt($this->time);
+		$out->putVarInt($this->eduEditionOffer);
+		$out->putBool($this->hasEduFeaturesEnabled);
+		$out->putString($this->eduProductUUID);
+		$out->putLFloat($this->rainLevel);
+		$out->putLFloat($this->lightningLevel);
+		$out->putBool($this->hasConfirmedPlatformLockedContent);
+		$out->putBool($this->isMultiplayerGame);
+		$out->putBool($this->hasLANBroadcast);
+		$out->putVarInt($this->xboxLiveBroadcastMode);
+		$out->putVarInt($this->platformBroadcastMode);
+		$out->putBool($this->commandsEnabled);
+		$out->putBool($this->isTexturePacksRequired);
+		$out->putGameRules($this->gameRules);
+		$this->experiments->write($out);
+		$out->putBool($this->hasBonusChestEnabled);
+		$out->putBool($this->hasStartWithMapEnabled);
+		$out->putVarInt($this->defaultPlayerPermission);
+		$out->putLInt($this->serverChunkTickRadius);
+		$out->putBool($this->hasLockedBehaviorPack);
+		$out->putBool($this->hasLockedResourcePack);
+		$out->putBool($this->isFromLockedWorldTemplate);
+		$out->putBool($this->useMsaGamertagsOnly);
+		$out->putBool($this->isFromWorldTemplate);
+		$out->putBool($this->isWorldTemplateOptionLocked);
+		$out->putBool($this->onlySpawnV1Villagers);
+		$out->putString($this->vanillaVersion);
+		$out->putLInt($this->limitedWorldWidth);
+		$out->putLInt($this->limitedWorldLength);
+		$out->putBool($this->isNewNether);
+		$this->eduSharedUriResource->write($out);
+		$out->putBool($this->experimentalGameplayOverride !== null);
+		if($this->experimentalGameplayOverride !== null){
+			$out->putBool($this->experimentalGameplayOverride);
+		}
+	}
+}

5285dde125b529e070db7eeb0d3774208e1652ef
Dylan K. Taylor <dktapps@pmmp.io>
2021-10-23 16:18:49 +0100

    PlayerActionPacket: move action constants to their own class

--- src/PlayerActionPacket.php
+++ src/PlayerActionPacket.php
@@ -27,40 +27,13 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 use pocketmine\network\mcpe\protocol\types\BlockPosition;
+use pocketmine\network\mcpe\protocol\types\PlayerAction;
 
 class PlayerActionPacket extends DataPacket implements ServerboundPacket{
 	public const NETWORK_ID = ProtocolInfo::PLAYER_ACTION_PACKET;
 
-	public const ACTION_START_BREAK = 0;
-	public const ACTION_ABORT_BREAK = 1;
-	public const ACTION_STOP_BREAK = 2;
-	public const ACTION_GET_UPDATED_BLOCK = 3;
-	public const ACTION_DROP_ITEM = 4;
-	public const ACTION_START_SLEEPING = 5;
-	public const ACTION_STOP_SLEEPING = 6;
-	public const ACTION_RESPAWN = 7;
-	public const ACTION_JUMP = 8;
-	public const ACTION_START_SPRINT = 9;
-	public const ACTION_STOP_SPRINT = 10;
-	public const ACTION_START_SNEAK = 11;
-	public const ACTION_STOP_SNEAK = 12;
-	public const ACTION_CREATIVE_PLAYER_DESTROY_BLOCK = 13;
-	public const ACTION_DIMENSION_CHANGE_ACK = 14; //sent when spawning in a different dimension to tell the server we spawned
-	public const ACTION_START_GLIDE = 15;
-	public const ACTION_STOP_GLIDE = 16;
-	public const ACTION_BUILD_DENIED = 17;
-	public const ACTION_CRACK_BREAK = 18;
-	public const ACTION_CHANGE_SKIN = 19;
-	public const ACTION_SET_ENCHANTMENT_SEED = 20; //no longer used
-	public const ACTION_START_SWIMMING = 21;
-	public const ACTION_STOP_SWIMMING = 22;
-	public const ACTION_START_SPIN_ATTACK = 23;
-	public const ACTION_STOP_SPIN_ATTACK = 24;
-	public const ACTION_INTERACT_BLOCK = 25;
-	public const ACTION_PREDICT_DESTROY_BLOCK = 26;
-	public const ACTION_CONTINUE_DESTROY_BLOCK = 27;
-
 	public int $actorRuntimeId;
+	/** @see PlayerAction */
 	public int $action;
 	public BlockPosition $blockPosition;
 	public int $face;
---
+++ src/types/PlayerAction.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol\types;
+
+final class PlayerAction{
+	private function __construct(){
+		//NOOP
+	}
+
+	public const START_BREAK = 0;
+	public const ABORT_BREAK = 1;
+	public const STOP_BREAK = 2;
+	public const GET_UPDATED_BLOCK = 3;
+	public const DROP_ITEM = 4;
+	public const START_SLEEPING = 5;
+	public const STOP_SLEEPING = 6;
+	public const RESPAWN = 7;
+	public const JUMP = 8;
+	public const START_SPRINT = 9;
+	public const STOP_SPRINT = 10;
+	public const START_SNEAK = 11;
+	public const STOP_SNEAK = 12;
+	public const CREATIVE_PLAYER_DESTROY_BLOCK = 13;
+	public const DIMENSION_CHANGE_ACK = 14; //sent when spawning in a different dimension to tell the server we spawned
+	public const START_GLIDE = 15;
+	public const STOP_GLIDE = 16;
+	public const BUILD_DENIED = 17;
+	public const CRACK_BREAK = 18;
+	public const CHANGE_SKIN = 19;
+	public const SET_ENCHANTMENT_SEED = 20; //no longer used
+	public const START_SWIMMING = 21;
+	public const STOP_SWIMMING = 22;
+	public const START_SPIN_ATTACK = 23;
+	public const STOP_SPIN_ATTACK = 24;
+	public const INTERACT_BLOCK = 25;
+	public const PREDICT_DESTROY_BLOCK = 26;
+	public const CONTINUE_DESTROY_BLOCK = 27;
+}

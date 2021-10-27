58c53a259e819a076bf8fe875d2a012da7d19d65
Dylan K. Taylor <dktapps@pmmp.io>
2021-10-27 20:49:20 +0100

    MovePlayerPacket: added create() and simple()

--- src/MovePlayerPacket.php
+++ src/MovePlayerPacket.php
@@ -48,6 +48,51 @@ class MovePlayerPacket extends DataPacket implements ClientboundPacket, Serverbo
 	public int $teleportItem = 0;
 	public int $tick = 0;
 
+	/**
+	 * @generate-create-func
+	 */
+	public static function create(
+		int $actorRuntimeId,
+		Vector3 $position,
+		float $pitch,
+		float $yaw,
+		float $headYaw,
+		int $mode,
+		bool $onGround,
+		int $ridingActorRuntimeId,
+		int $teleportCause,
+		int $teleportItem,
+		int $tick,
+	) : self{
+		$result = new self;
+		$result->actorRuntimeId = $actorRuntimeId;
+		$result->position = $position;
+		$result->pitch = $pitch;
+		$result->yaw = $yaw;
+		$result->headYaw = $headYaw;
+		$result->mode = $mode;
+		$result->onGround = $onGround;
+		$result->ridingActorRuntimeId = $ridingActorRuntimeId;
+		$result->teleportCause = $teleportCause;
+		$result->teleportItem = $teleportItem;
+		$result->tick = $tick;
+		return $result;
+	}
+
+	public static function simple(
+		int $actorRuntimeId,
+		Vector3 $position,
+		float $pitch,
+		float $yaw,
+		float $headYaw,
+		int $mode,
+		bool $onGround,
+		int $ridingActorRuntimeId,
+		int $tick,
+	) : self{
+		return self::create($actorRuntimeId, $position, $pitch, $yaw, $headYaw, $mode, $onGround, $ridingActorRuntimeId, 0, 0, $tick);
+	}
+
 	protected function decodePayload(PacketSerializer $in) : void{
 		$this->actorRuntimeId = $in->getActorRuntimeId();
 		$this->position = $in->getVector3();

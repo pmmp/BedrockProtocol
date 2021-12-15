410b23c3da271e157bbf7510923b97d4ba0c1f78
Dylan K. Taylor <dktapps@pmmp.io>
2021-12-15 04:08:22 +0000

    PlayerMovementType: give V2_REWIND a more fair description comment
    As it turns out, I was horribly, horribly wrong about rewind being useless - it's actually useful for server-authoritative knockback, since it enables the server and client to negotiate positions without spamming movement packets at each other.

--- src/types/PlayerMovementType.php
+++ src/types/PlayerMovementType.php
@@ -27,5 +27,5 @@ final class PlayerMovementType{
 
 	public const LEGACY = 0; //MovePlayerPacket
 	public const SERVER_AUTHORITATIVE_V1 = 1; //PlayerAuthInputPacket
-	public const SERVER_AUTHORITATIVE_V2_REWIND = 2; //PlayerAuthInputPacket + a bunch of junk that solves a nonexisting problem
+	public const SERVER_AUTHORITATIVE_V2_REWIND = 2; //PlayerAuthInputPacket + movement replay (used for server authoritative knockback)
 }

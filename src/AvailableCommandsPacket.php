83462013f1833d08ec9bd8194a9937d59523f76f
Dylan K. Taylor <dktapps@pmmp.io>
2022-06-08 01:52:19 +0100

    AvailableCommandsPacket: updated argument type constants

--- src/AvailableCommandsPacket.php
+++ src/AvailableCommandsPacket.php
@@ -42,25 +42,30 @@ class AvailableCommandsPacket extends DataPacket implements ClientboundPacket{
 	public const ARG_TYPE_VALUE = 0x04;
 	public const ARG_TYPE_WILDCARD_INT = 0x05;
 	public const ARG_TYPE_OPERATOR = 0x06;
-	public const ARG_TYPE_TARGET = 0x07;
-	public const ARG_TYPE_WILDCARD_TARGET = 0x09;
+	public const ARG_TYPE_COMPARE_OPERATOR = 0x07;
+	public const ARG_TYPE_TARGET = 0x08;
 
-	public const ARG_TYPE_FILEPATH = 0x10;
+	public const ARG_TYPE_WILDCARD_TARGET = 0x0a;
 
-	public const ARG_TYPE_EQUIPMENT_SLOT = 0x25;
+	public const ARG_TYPE_FILEPATH = 0x11;
 
-	public const ARG_TYPE_STRING = 0x26;
+	public const ARG_TYPE_FULL_INTEGER_RANGE = 0x17;
 
-	public const ARG_TYPE_INT_POSITION = 0x2e;
-	public const ARG_TYPE_POSITION = 0x2f;
+	public const ARG_TYPE_EQUIPMENT_SLOT = 0x26;
+	public const ARG_TYPE_STRING = 0x27;
 
-	public const ARG_TYPE_MESSAGE = 0x32;
+	public const ARG_TYPE_INT_POSITION = 0x2f;
+	public const ARG_TYPE_POSITION = 0x30;
 
-	public const ARG_TYPE_RAWTEXT = 0x34;
+	public const ARG_TYPE_MESSAGE = 0x33;
 
-	public const ARG_TYPE_JSON = 0x38;
+	public const ARG_TYPE_RAWTEXT = 0x35;
 
-	public const ARG_TYPE_COMMAND = 0x45;
+	public const ARG_TYPE_JSON = 0x39;
+
+	public const ARG_TYPE_BLOCK_STATES = 0x43;
+
+	public const ARG_TYPE_COMMAND = 0x46;
 
 	/**
 	 * Enums are a little different: they are composed as follows:

69a309a2dd7dcf3ec8c316385b866397e8c2cbfd
Dylan K. Taylor <dktapps@pmmp.io>
2023-07-24 16:35:36 +0100

    UIInventorySlotOffset: added missing mapping for smithing table template slot this is a tad confusing, as it's the leftmost slot.

--- src/types/inventory/UIInventorySlotOffset.php
+++ src/types/inventory/UIInventorySlotOffset.php
@@ -14,6 +14,9 @@ declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types\inventory;
 
+/**
+ * Values extracted from PlayerUISlot enum in Bedrock
+ */
 final class UIInventorySlotOffset{
 
 	private function __construct(){
@@ -94,7 +97,8 @@ final class UIInventorySlotOffset{
 	];
 	public const CREATED_ITEM_OUTPUT = 50;
 	public const SMITHING_TABLE = [
-		51 => 0,
-		52 => 1,
+		51 => 0, //input
+		52 => 1, //material
+		53 => 2, //template
 	];
 }

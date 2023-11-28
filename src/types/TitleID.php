fedef75ea47fa61845262aaa81a5d624d8bd6fe8
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-11-28 10:10:17 -0500

    add title id consts
    all known bedrock title ids

---
+++ src/types/TitleID.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of BedrockProtocol.
+ * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
+ *
+ * BedrockProtocol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe\protocol\types;
+
+final class TitleID{
+
+	public const ANDROID = 1739947436;
+	public const IOS = 1810924247;
+	public const AMAZON = 1944307183;
+	public const WINDOWS_10 = 896928775;
+	public const PLAYSTATION = 2044456598;
+	public const NINTENDO = 2047319603;
+	public const XBOX = 1828326430;
+	public const PREVIEW = 1904044383;
+
+}

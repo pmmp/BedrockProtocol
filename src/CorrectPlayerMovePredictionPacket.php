10e34be8816586abde69525016322abb98c87e4a
Tobias Grether <me@tobiasgrether.com>
2024-03-01 22:42:39 +0100

    feat: implement movement prediction type

--- src/CorrectPlayerMovePredictionPacket.php
+++ src/CorrectPlayerMovePredictionPacket.php
@@ -20,20 +20,25 @@ use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 class CorrectPlayerMovePredictionPacket extends DataPacket implements ClientboundPacket{
 	public const NETWORK_ID = ProtocolInfo::CORRECT_PLAYER_MOVE_PREDICTION_PACKET;
 
+	public const PREDICTION_TYPE_VEHICLE = 0;
+	public const PREDICTION_TYPE_PLAYER = 1;
+
 	private Vector3 $position;
 	private Vector3 $delta;
 	private bool $onGround;
 	private int $tick;
+	private int $predictionType;
 
 	/**
 	 * @generate-create-func
 	 */
-	public static function create(Vector3 $position, Vector3 $delta, bool $onGround, int $tick) : self{
+	public static function create(Vector3 $position, Vector3 $delta, bool $onGround, int $tick, int $predictionType) : self{
 		$result = new self;
 		$result->position = $position;
 		$result->delta = $delta;
 		$result->onGround = $onGround;
 		$result->tick = $tick;
+		$result->predictionType = $predictionType;
 		return $result;
 	}
 
@@ -45,11 +50,14 @@ class CorrectPlayerMovePredictionPacket extends DataPacket implements Clientboun
 
 	public function getTick() : int{ return $this->tick; }
 
+	public function getPredictionType() : int{ return $this->predictionType; }
+
 	protected function decodePayload(PacketSerializer $in) : void{
 		$this->position = $in->getVector3();
 		$this->delta = $in->getVector3();
 		$this->onGround = $in->getBool();
 		$this->tick = $in->getUnsignedVarLong();
+		$this->predictionType = $in->getByte();
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
@@ -57,6 +65,7 @@ class CorrectPlayerMovePredictionPacket extends DataPacket implements Clientboun
 		$out->putVector3($this->delta);
 		$out->putBool($this->onGround);
 		$out->putUnsignedVarLong($this->tick);
+		$out->putByte($this->predictionType);
 	}
 
 	public function handle(PacketHandlerInterface $handler) : bool{

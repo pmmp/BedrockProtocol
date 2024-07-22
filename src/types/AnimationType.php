08880c1c631a45aa224f3c7215db34674c926006
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-22 17:19:24 +0700

    Adding Licence Headers

--- src/types/AnimationType.php
+++ src/types/AnimationType.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of BedrockProtocol.
+ * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
+ *
+ * BedrockProtocol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types;

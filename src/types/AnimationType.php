d0c334a146aefba87e12ba7cb6b8f694a5a50a47
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-22 17:38:15 +0700

    Update AnimationType.php

--- src/types/AnimationType.php
+++ src/types/AnimationType.php
@@ -15,13 +15,13 @@ declare(strict_types=1);
 namespace pocketmine\network\mcpe\protocol\types;
 
 class AnimationType {
-    public const SWIM = "minecraft:swim";
-    public const WALK = "minecraft:walk";
-    public const RUN = "minecraft:run";
-    public const JUMP = "minecraft:jump";
-    public const ATTACK = "minecraft:attack";
-    public const HURT = "minecraft:hurt";
-    public const DEATH = "minecraft:death";
-    public const EAT = "minecraft:eat";
-    public const SLEEP = "minecraft:sleep";
+	public const SWIM = "minecraft:swim";
+	public const WALK = "minecraft:walk";
+	public const RUN = "minecraft:run";
+	public const JUMP = "minecraft:jump";
+	public const ATTACK = "minecraft:attack";
+	public const HURT = "minecraft:hurt";
+	public const DEATH = "minecraft:death";
+	public const EAT = "minecraft:eat";
+	public const SLEEP = "minecraft:sleep";
 }

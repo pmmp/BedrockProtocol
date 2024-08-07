b472b7be8c130da9619662fcfe4a1338b6d1486e
IvanCraft623 <ivancraft623@gmail.com>
2024-08-07 17:55:38 -0500

    Update VISIBLE_MOB_EFFECTS doc

--- src/types/entity/EntityMetadataProperties.php
+++ src/types/entity/EntityMetadataProperties.php
@@ -160,5 +160,5 @@ final class EntityMetadataProperties{
 	public const PLAYER_DEATH_DIMENSION = 128; //int
 	public const PLAYER_HAS_DIED = 129; //byte
 	public const COLLISION_BOX = 130; //compound
-	public const VISIBLE_MOB_EFFECTS = 131; //int64 (bytes array of effect ids)
+	public const VISIBLE_MOB_EFFECTS = 131; //long (first 8 visible effects packed, 6 bits for effectId + 1 bit for ambient indicator)
 }

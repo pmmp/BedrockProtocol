fdf247ccc3b964a2c200093e033a81af7ed55853
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-12-03 18:36:30 +0700

    Update ItemStackWrapper.php

--- src/types/inventory/ItemStackWrapper.php
+++ src/types/inventory/ItemStackWrapper.php
@@ -16,7 +16,7 @@ namespace pocketmine\network\mcpe\protocol\types\inventory;
 
 final class ItemStackWrapper{
 	public function __construct(
-		private int $stackId,
+		private int $itemStackId,
 		private ItemStack $itemStack
 	){}
 
@@ -24,7 +24,7 @@ final class ItemStackWrapper{
 		return new self($itemStack->getId() === 0 ? 0 : 1, $itemStack);
 	}
 
-	public function getStackId() : int{ return $this->stackId; }
+	public function getItemStackId() : int{ return $this->itemStackId; }
 
 	public function getItemStack() : ItemStack{ return $this->itemStack; }
 }

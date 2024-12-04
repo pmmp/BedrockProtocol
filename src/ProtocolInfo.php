b12daddf4c2f963b6d7b43decfc2524da06fd85a
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-04 17:27:14 +0700

    Update ProtocolInfo.php

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -32,11 +32,11 @@ final class ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 748;
+	public const CURRENT_PROTOCOL = 766;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.21.40';
+	public const MINECRAFT_VERSION = 'v1.21.50';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.21.40';
+	public const MINECRAFT_VERSION_NETWORK = '1.21.50';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;

3549f7488ee072275834197404f8d3a164f19372
Dries C <coenendries3@gmail.com>
2025-03-27 15:41:24 +0100

    Add documentation to OverrideUpdateType

--- src/types/OverrideUpdateType.php
+++ src/types/OverrideUpdateType.php
@@ -14,6 +14,9 @@ declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types;
 
+/**
+ * @see PlayerUpdateEntityOverridesPacket
+ */
 enum OverrideUpdateType : int{
 	use PacketIntEnumTrait;
 

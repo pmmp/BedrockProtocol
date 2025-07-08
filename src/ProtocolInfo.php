e9bc5fb691d18dab229a158462c13f0c6fea79c8
Dries C <15795262+dries-c@users.noreply.github.com>
2025-07-08 14:31:39 +0200

    Protocol changes for 1.21.93 (#317)

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -32,11 +32,11 @@ final class ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 818;
+	public const CURRENT_PROTOCOL = 819;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.21.90';
+	public const MINECRAFT_VERSION = 'v1.21.93';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.21.90';
+	public const MINECRAFT_VERSION_NETWORK = '1.21.93';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;

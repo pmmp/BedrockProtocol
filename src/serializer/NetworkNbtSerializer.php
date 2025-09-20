1f6892bf81f209de8ae99618a1e5ec5b4d40a05a
Dylan K. Taylor <dktapps@pmmp.io>
2025-09-20 20:57:13 +0100

    Switch back to stable NBT
    The current game plan is to roll out BedrockProtocol only with ext-encoding first. This ensures that
    if there are any major issues that my (extensive) unit tests haven't caught, the damage will be
    limited to packet encoding, so no permanent damage will be done.
    
    Since we use NBT in world saves as well as on the network, depending on the version using
    ext-encoding here would make this next upgrade very risky, so it's not worth it for now. The time
    for that will come in the next month or two.

--- src/serializer/NetworkNbtSerializer.php
+++ src/serializer/NetworkNbtSerializer.php
@@ -14,8 +14,6 @@ declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\serializer;
 
-use pmmp\encoding\LE;
-use pmmp\encoding\VarInt;
 use pocketmine\nbt\BaseNbtSerializer;
 use pocketmine\nbt\NbtDataException;
 use function count;
@@ -24,75 +22,75 @@ use function strlen;
 class NetworkNbtSerializer extends BaseNbtSerializer{
 
 	public function readShort() : int{
-		return LE::readUnsignedShort($this->reader);
+		return $this->buffer->getLShort();
 	}
 
 	public function readSignedShort() : int{
-		return LE::readSignedShort($this->reader);
+		return $this->buffer->getSignedLShort();
 	}
 
 	public function writeShort(int $v) : void{
-		//TODO: signed and unsigned are encoded the same, but this API should be redesigned
-		LE::writeSignedShort($this->writer, $v);
+		$this->buffer->putLShort($v);
 	}
 
 	public function readInt() : int{
-		return VarInt::readSignedInt($this->reader);
+		return $this->buffer->getVarInt();
 	}
 
 	public function writeInt(int $v) : void{
-		VarInt::writeSignedInt($this->writer, $v);
+		$this->buffer->putVarInt($v);
 	}
 
 	public function readLong() : int{
-		return VarInt::readSignedLong($this->reader);
+		return $this->buffer->getVarLong();
 	}
 
 	public function writeLong(int $v) : void{
-		VarInt::writeSignedLong($this->writer, $v);
+		$this->buffer->putVarLong($v);
 	}
 
 	public function readString() : string{
-		return $this->reader->readByteArray(self::checkReadStringLength(VarInt::readUnsignedInt($this->reader)));
+		return $this->buffer->get(self::checkReadStringLength($this->buffer->getUnsignedVarInt()));
 	}
 
 	public function writeString(string $v) : void{
-		VarInt::writeUnsignedInt($this->writer, self::checkWriteStringLength(strlen($v)));
-		$this->writer->writeByteArray($v);
+		$this->buffer->putUnsignedVarInt(self::checkWriteStringLength(strlen($v)));
+		$this->buffer->put($v);
 	}
 
 	public function readFloat() : float{
-		return LE::readFloat($this->reader);
+		return $this->buffer->getLFloat();
 	}
 
 	public function writeFloat(float $v) : void{
-		LE::writeFloat($this->writer, $v);
+		$this->buffer->putLFloat($v);
 	}
 
 	public function readDouble() : float{
-		return LE::readDouble($this->reader);
+		return $this->buffer->getLDouble();
 	}
 
 	public function writeDouble(float $v) : void{
-		LE::writeDouble($this->writer, $v);
+		$this->buffer->putLDouble($v);
 	}
 
 	public function readIntArray() : array{
-		$len = VarInt::readSignedInt($this->reader);
+		$len = $this->readInt(); //varint
 		if($len < 0){
 			throw new NbtDataException("Array length cannot be less than zero ($len < 0)");
 		}
-		$result = [];
+		$ret = [];
 		for($i = 0; $i < $len; ++$i){
-			$result[] = VarInt::readSignedInt($this->reader);
+			$ret[] = $this->readInt(); //varint
 		}
-		return $result;
+
+		return $ret;
 	}
 
 	public function writeIntArray(array $array) : void{
-		VarInt::writeSignedInt($this->writer, count($array));
+		$this->writeInt(count($array)); //varint
 		foreach($array as $v){
-			VarInt::writeSignedInt($this->writer, $v);
+			$this->writeInt($v); //varint
 		}
 	}
 }

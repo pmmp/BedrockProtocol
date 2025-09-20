2bdaa446119cab96b37f688e575a706fd92e5239
ismail <dev.1smail.eke34@gmail.com>
2025-09-20 16:32:49 +0300

    Typos and forgotten event types

--- src/LegacyTelemetryEventPacket.php
+++ src/LegacyTelemetryEventPacket.php
@@ -30,7 +30,7 @@ class LegacyTelemetryEventPacket extends DataPacket implements ClientboundPacket
 	public const TYPE_AGENT_COMMAND = 8;
 	public const TYPE_AGENT_CREATED = 9;
 	public const TYPE_PATTERN_REMOVED = 10; //???
-	public const TYPE_COMMANED_EXECUTED = 11;
+	public const TYPE_SLASH_COMMAND_EXECUTED = 11;
 	public const TYPE_FISH_BUCKETED = 12;
 	public const TYPE_MOB_BORN = 13;
 	public const TYPE_PET_DIED = 14;
@@ -44,6 +44,13 @@ class LegacyTelemetryEventPacket extends DataPacket implements ClientboundPacket
 	public const TYPE_HONEY_HARVESTED = 22;
 	public const TYPE_TARGET_BLOCK_HIT = 23;
 	public const TYPE_PIGLIN_BARTER = 24;
+	public const TYPE_PLAYER_WAXED_OR_UNWAXED_COPPER = 25;
+	public const TYPE_CODE_BUILDER_RUNTIME_ACTION = 26;
+	public const TYPE_CODE_BUILDER_SCOREBOARD = 27;
+	public const TYPE_STRIDER_RIDDEN_IN_LAVA_IN_OVERWORLD = 28;
+	public const TYPE_SNEAK_CLOSE_TO_SCULK_SENSOR = 29;
+	public const TYPE_CAREFUL_RESTORATION = 30;
+	public const TYPE_ITEM_USED = 31;
 
 	public int $playerRuntimeId;
 	public int $eventData;

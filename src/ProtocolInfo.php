2e7633fb4029143257f4a8c68f8684b8cd31d8fc
Dylan K. Taylor <dktapps@pmmp.io>
2025-10-02 16:31:57 +0100

    Bring protocol version in line with generated data

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -34,9 +34,9 @@ final class ProtocolInfo{
 	/** Actual Minecraft: PE protocol version */
 	public const CURRENT_PROTOCOL = 844;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.21.110';
+	public const MINECRAFT_VERSION = 'v1.21.111';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.21.110';
+	public const MINECRAFT_VERSION_NETWORK = '1.21.111';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;

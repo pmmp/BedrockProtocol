ed39afa24a5eed7d5a8c434223f2bd496e1fe011
dries-c <15795262+dries-c@users.noreply.github.com>
2025-12-09 22:27:22 +0100

    Bump protocol version, close #336

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -32,7 +32,7 @@ final class ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 897;
+	public const CURRENT_PROTOCOL = 898;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
 	public const MINECRAFT_VERSION = 'v1.21.130';
 	/** Version number sent to clients in ping responses. */
